d9ea67f4a565b30cc80175dcdc9f43532ccfec73
Zak Monnet <zakmonnet@gmail.com>
2021-01-04 21:45:42 -0600

    edited components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Chat from "./components/Chat/Chat";
+import Group from "./pages/Group";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <div className="app__body">
-          <Switch>
-            <Route exact path="/">
-              <Sidebar />
-              <Chat />
-            </Route>
-            <Route exact path="/signup"></Route>
-            <Route exact path="/user/:id"></Route>
-            <Route exact path="/group/:id"></Route>
-          </Switch>
-        </div>
+        <Switch>
+          <Route exact path="/">
+            <Group />
+          </Route>
+          <Route exact path="/signup"></Route>
+          <Route exact path="/user/:id"></Route>
+          <Route exact path="/group/:id">
+            <Group />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
