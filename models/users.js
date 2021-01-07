a3e5a5ec2917bde5884fe14ac9e4615c0934107c
Zak Monnet <zakmonnet@gmail.com>
2021-01-06 20:42:32 -0600

    fixed password hashing

--- models/users.js
+++ models/users.js
@@ -3,14 +3,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    first_name: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-    last_name: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-    email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-    password: { type: String, required: true },
-    password2: { type: String, required: true },
-    groups: [{ type: Schema.Types.ObjectId, ref: "group" }],
-    subgroups: [{ type: Schema.Types.ObjectId, ref: "group"}]
-
+  first_name: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+    index: true,
+  },
+  last_name: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+    index: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/\S+@\S+\.\S+/, "is invalid"],
+    index: true,
+  },
+  password: { type: String, required: true },
+  groups: [{ type: Schema.Types.ObjectId, ref: "group" }],
+  subgroups: [{ type: Schema.Types.ObjectId, ref: "group" }],
 });
 
 const User = mongoose.model("user", userSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const LocalStrategy = require("passport-local").Strategy
+const express = require("express");
+const LocalStrategy = require("passport-local").Strategy;
 
 const router = require("express").Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 const userController = require("../../controller/userController");
 const User = require("../../models/users");
 const { forwardAuthenticated } = require("../../config/auth");
 
-
 // Matches with "/api/users"
-router.route("/").get(userController.findAll)
-router.route("/").post(userController.create);
+//router.route("/").get(userController.findAll);
+//router.route("/").post(userController.create);
 
-router.route("api/users")
-.get(userController.login)
-.post(userController.register)
+router
+  .route("/")
+  //.get(userController.login)
+  .post(userController.register);
 
 // Matches with "/api/users/:id"
 router
@@ -23,8 +23,5 @@ router
   .get(userController.findById)
   .put(userController.update)
   .delete(userController.remove);
-
-
-
 
 module.exports = router;
