ecad7bb7c3553dda8d514df11bb9ca1f133470d1
Andrea Boyd <andreanboyd@yahoo.com>
2021-01-08 16:06:49 -0600

    build message display functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Chat from "./components/Chat/Chat";
 import SignUp from "./pages/Signup";
-
+import Group from "./pages/Group"
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route exact path="/">
-            <Sidebar />
-            <Chat />
+          
           </Route>
           <Route exact path="/signup">
             <SignUp />
           </Route>
           <Route exact path="/user/"></Route>
-          <Route exact path="/group">
-            <div className="app__body">
-              <Sidebar />
-              <Chat />
-            </div>
+          <Route exact path="/group">            
+              <Group />       
           </Route>
         </Switch>
       </div>
