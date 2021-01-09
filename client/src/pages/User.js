6d257123434ab747dd24cda6d2fac35d669fa7b1
Kevin Pulley <skippy1025@gmail.com>
2021-01-08 18:50:01 -0600

    Auto stash before merge of "Kevin" and "origin/main"
    Got button displaying from object in User.js

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -5,50 +5,71 @@ import "./User.css";
 function User() {
   const [groups, setGroups] = useState({}); //groups that users are part of
 
+  let groupsTest = [
+    { name: "Javascript", _id: "1" },
+    { name: "React", _id: "2" },
+  ];
+
   useEffect(() => {
     loadGroups();
   }, []);
 
   function loadGroups() {
-    API.loadGroup({});
+    API.getUser()
+      .then((res) => setGroups(res.data))
+      .catch((err) => console.log(err));
   }
 
-  function handleInputChange(event){
-const {name, value} = event.target;
-setGroups({...groups, [name]: value})
+  function handleInputChange(event) {
+    console.log(event.target.value);
+    const { name, value } = event.target;
+    setGroups({ ...groups, [name]: value });
   }
 
-
-  const handleSubmit = (e) => {
+  function handleFormSubmit(e) {
     e.preventDefault();
+    console.log(groups);
     API.saveGroup({
       name: groups.groupName,
-    })
-};
-  
-  
+    });
+  }
+
+
 
   return (
-    <div user__container>
-      <form>
-        <input
-          onSubmit={handleSubmit}
-          className="user-form-control"
-          type="text"
-          placeholder="Enter A New Group"
-          name="groupName"
-          onChange={handleInputChange}
-        />
+    <>
+      <div className="user__container">
+        <form>
+          <input
+            onSubmit={handleFormSubmit}
+            className="user-form-control"
+            type="text"
+            placeholder="Enter A New Group"
+            name="groupName"
+            onChange={handleInputChange}
+          />
 
-        <button onClick="" className="user__btn">
-      
-        </button>
-      </form>
+          <button onClick={handleFormSubmit} className="user__btn">
+            Submit
+          </button>
+        </form>
 
-      <div>
-        <button>{groups}</button>
+        <div>
+          {groupsTest.map((btn) => (
+            <Link to="">
+              <button
+                key={btn._id}
+                className="group__btn"
+                value={btn._id}
+                
+              >
+                {btn.name}
+              </button>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const groupRoutes = require("./groups");
 
+
+router.use("/groups", groupRoutes);
 // User Routes
 router.use("/users", userRoutes);
 
