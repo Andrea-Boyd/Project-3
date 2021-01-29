b1c1048950fb15b12a0e11cf81c0bf6b36b12cf0
Zak Monnet <zakmonnet@gmail.com>
2021-01-29 10:42:57 -0600

    edited comments/console logs in server code

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,3 @@
-// module.exports = function(req, res, next) {
-//     // If the user is logged in, continue with the request to the restricted route
-//     if (req.user) {
-//       return next();
-//     }
-
-//     // If the user isn't logged in, redirect them to the login page
-//     return res.redirect("/");
-//   };
-
 module.exports.isAuth = (req, res, next) => {
   if (req.user) {
     next();
